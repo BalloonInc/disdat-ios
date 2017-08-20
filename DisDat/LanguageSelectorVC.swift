bc83ce24c60ebfaeed43f1e6d53c5bca92a0a67f
Wouter Devriendt <wouter@ballooninc.be>
2017-08-20 21:13:05 +0200

    language selector UI

--- DisDat/LanguageSelectorVC.swift
+++ DisDat/LanguageSelectorVC.swift
@@ -14,6 +14,17 @@ class LanguageSelectorVC: UIViewController {
     @IBOutlet weak var newLanguagePicker: LanguageSelectionPickerView!
     @IBOutlet weak var modelSelector: UISegmentedControl!
     
+    @IBOutlet weak var nativeLanguageTile: UIView!
+    @IBOutlet weak var learningLanguageTile: UIView!
+    @IBOutlet weak var readyTile: UIView!
+    
+    
+    override func viewDidLoad() {
+        setTileUI(tile: nativeLanguageTile, id: 1)
+        setTileUI(tile: learningLanguageTile, id: 2)
+        setTileUI(tile: readyTile, id: 3)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "languageSelectedSegue"){
             if let destination = segue.destination as? TranslateVC {
@@ -24,5 +35,21 @@ class LanguageSelectorVC: UIViewController {
                 DiscoveredWordCollection.setLanguages(rootLanguage: rootLanguagePicker.selectedLanguageCode, learningLanguage: newLanguagePicker.selectedLanguageCode)
             }
         }
+    }
+    
+    func setTileUI(tile: UIView, id: Int){
+        tile.layer.cornerRadius = 5;
+        tile.layer.masksToBounds = false;
+        
+        let circleView = UIView(frame: CGRect(x: tile.frame.origin.x + tile.frame.width/2, y: tile.frame.origin.y, width: 50, height: 50))
+
+        circleView.layer.cornerRadius = 45;  // half the width/height
+        circleView.backgroundColor = UIColor.yellow
+        tile.addSubview(circleView)
+        
+        let label = UILabel(frame: circleView.frame)
+        label.text = "\(id)"
+        
+        circleView.addSubview(label)
     }
 }
