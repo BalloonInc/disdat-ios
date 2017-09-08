0c12b370e3d01e495ebf4cd812fb27573d9e6afe
Wouter Devriendt <wouterdevriendt@gmail.com>
2017-09-08 15:42:42 +0200

    stuffstuff

--- DisDat/ViewControllers/UndiscoveredCardVC.swift
+++ DisDat/ViewControllers/UndiscoveredCardVC.swift
@@ -11,10 +11,18 @@ import UIKit
 class UndiscoveredCardVC: UIViewController {
 
     @IBOutlet weak var unknownCardLabel: UILabel!
+    @IBOutlet weak var cardView: UIView?
     
     var unknownCount = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cardView?.layer.shadowColor = #colorLiteral(red: 0.1490027606, green: 0.1490303874, blue: 0.1489966214, alpha: 1)
+        
+        cardView?.layer.shadowOpacity = 0.3;
+        cardView?.layer.shadowRadius = 1.0;
+        cardView?.layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
+        
         if unknownCount > 0 {
             unknownCardLabel.text = String(format: NSLocalizedString("Still %d undiscovered words in this category. Go out and scan some!", comment: ""), unknownCount)
         }
